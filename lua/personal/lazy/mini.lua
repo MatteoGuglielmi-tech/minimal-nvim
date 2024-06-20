bbcafd08e246ee9634d99f2d397a234172257278
MatteoGuglielmi-Tech <matteo.guglielmi.ma.gu@gmail.com>
2024-06-20 19:01:41 +0200

    build(mini-bookmarks): update keymappings

--- lua/personal/lazy/mini.lua
+++ lua/personal/lazy/mini.lua
@@ -14,12 +14,12 @@ return {
 
 		require("mini.surround").setup({
 			mappings = {
-				add = "sa", -- Add surrounding in Normal and Visual modes
-				delete = "sd", -- Delete surrounding
+				add = "as", -- Add surrounding in Normal and Visual modes
+				delete = "ds", -- Delete surrounding
 				find = "sf", -- Find surrounding (to the right)
 				find_left = "sF", -- Find surrounding (to the left)
 				highlight = "sH", -- Highlight surrounding
-				replace = "sr", -- Replace surrounding
+				replace = "rs", -- Replace surrounding
 				update_n_lines = "sn", -- Update `n_lines`
 
 				suffix_last = "l", -- Suffix to search with "prev" method
