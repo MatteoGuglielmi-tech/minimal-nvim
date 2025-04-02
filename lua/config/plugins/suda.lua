9867f80cb8c3bab2a0972b3208d51a1a65eb8c4c
MatteoGuglielmi-Tech <matteo.guglielmi.ma.gu@gmail.com>
2025-04-02 11:24:55 +0200

    build(suda): read and save files with sudo priviledges

---
+++ lua/config/plugins/suda.lua
@@ -0,0 +1,15 @@
+return {
+	"lambdalisue/vim-suda",
+	keys = {
+		{
+			"<leader>sr",
+			"<cmd>SudaRead<CR>",
+			desc = "[S]uda[R]ead",
+		},
+		{
+			"<leader>sw",
+			"<cmd>SudaWrite<CR>",
+			desc = "[S]uda[W]rite",
+		},
+	},
+}
