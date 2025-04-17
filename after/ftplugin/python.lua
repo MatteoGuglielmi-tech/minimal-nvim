90c14d4bc10be15e6ef915539b7bd542a13a9866
MatteoGuglielmi-Tech <matteo.guglielmi.ma.gu@gmail.com>
2025-04-17 21:35:00 +0200

    chore: cleanup

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,14 +1,8 @@
+---@diagnostic disable: unused-local
 vim.opt_local.expandtab = true
 vim.opt_local.shiftwidth = 4
 vim.opt_local.softtabstop = 4
 vim.opt_local.formatoptions:append({ c = true, r = true, o = true, q = true })
-
-vim.keymap.set(
-	"n",
-	"<leader>ce",
-	"<cmd>lua require('swenv.api').pick_venv()<CR>",
-	{ desc = "Choose python environment" }
-)
 
 -- terminal
 local job_id = 0
@@ -19,7 +13,3 @@ vim.keymap.set("n", "<space>st", function()
 	vim.api.nvim_win_set_height(0, 15)
 	job_id = vim.bo.channel
 end, { desc = "Add terminal split" })
-
-vim.keymap.set("n", "<leader>ct", function()
-	vim.fn.chansend(job_id, { "py main.py" })
-end)
--- lua/config/plugins/lint.lua
+++ lua/config/plugins/lint.lua
@@ -3,9 +3,7 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
 		require("lint").linters_by_ft = {
-			-- c = { "cpplint" },
 			gitcommit = { "commitlint" },
-			markdown = { "markdownlint" },
 			bash = { "shellcheck" },
 			zsh = { "zsh" },
 		}
