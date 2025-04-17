dd87bc6a16e569c1ab47bcbfd81ab10f06475427
MatteoGuglielmi-Tech <matteo.guglielmi.ma.gu@gmail.com>
2025-04-17 21:49:47 +0200

    chore(treesitter): remove comments, add parenthesis

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -6,7 +6,8 @@ return {
 	version = false,
 	build = ":TSUpdate",
 	event = { "BufReadPre", "BufNewFile" },
-	lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+	-- load treesitter early when opening a file from the cmdline
+	lazy = vim.fn.argc(-1) == 0,
 	init = function(plugin)
 		require("nvim-treesitter.query_predicates")
 	end,
@@ -39,6 +40,7 @@ return {
 				node_decremental = "<bs>",
 			},
 		},
+
 		textobjects = {
 			select = {
 				enable = true,
@@ -92,39 +94,39 @@ return {
 
 				move = {
 					enable = true,
-					set_jumps = true, -- whether to set jumps in the jumplist
+					set_jumps = true,
 					goto_next_start = {
-						["]f"] = { query = "@call.outer", desc = "Next function call start" },
-						["]m"] = { query = "@function.outer", desc = "Next method/function def start" },
-						["]c"] = { query = "@class.outer", desc = "Next class start" },
-						["]i"] = { query = "@conditional.outer", desc = "Next conditional start" },
-						["]l"] = { query = "@loop.outer", desc = "Next loop start" },
+						["]f"] = { query = "@call.outer", { desc = "Next function call start" } },
+						["]m"] = { query = "@function.outer", { desc = "Next method/function def start" } },
+						["]c"] = { query = "@class.outer", { desc = "Next class start" } },
+						["]i"] = { query = "@conditional.outer", { desc = "Next conditional start" } },
+						["]l"] = { query = "@loop.outer", { desc = "Next loop start" } },
 
 						-- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
 						-- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
-						["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
-						["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+						["]s"] = { query = "@scope", query_group = "locals", { desc = "Next scope" } },
+						["]z"] = { query = "@fold", query_group = "folds", { desc = "Next fold" } },
 					},
 					goto_next_end = {
-						["]F"] = { query = "@call.outer", desc = "Next function call end" },
-						["]M"] = { query = "@function.outer", desc = "Next method/function def end" },
-						["]C"] = { query = "@class.outer", desc = "Next class end" },
-						["]I"] = { query = "@conditional.outer", desc = "Next conditional end" },
-						["]L"] = { query = "@loop.outer", desc = "Next loop end" },
+						["]F"] = { query = "@call.outer", { desc = "Next function call end" } },
+						["]M"] = { query = "@function.outer", { desc = "Next method/function def end" } },
+						["]C"] = { query = "@class.outer", { desc = "Next class end" } },
+						["]I"] = { query = "@conditional.outer", { desc = "Next conditional end" } },
+						["]L"] = { query = "@loop.outer", { desc = "Next loop end" } },
 					},
 					goto_previous_start = {
-						["[f"] = { query = "@call.outer", desc = "Prev function call start" },
-						["[m"] = { query = "@function.outer", desc = "Prev method/function def start" },
-						["[c"] = { query = "@class.outer", desc = "Prev class start" },
-						["[i"] = { query = "@conditional.outer", desc = "Prev conditional start" },
-						["[l"] = { query = "@loop.outer", desc = "Prev loop start" },
+						["[f"] = { query = "@call.outer", { desc = "Prev function call start" } },
+						["[m"] = { query = "@function.outer", { desc = "Prev method/function def start" } },
+						["[c"] = { query = "@class.outer", { desc = "Prev class start" } },
+						["[i"] = { query = "@conditional.outer", { desc = "Prev conditional start" } },
+						["[l"] = { query = "@loop.outer", { desc = "Prev loop start" } },
 					},
 					goto_previous_end = {
-						["[F"] = { query = "@call.outer", desc = "Prev function call end" },
-						["[M"] = { query = "@function.outer", desc = "Prev method/function def end" },
-						["[C"] = { query = "@class.outer", desc = "Prev class end" },
-						["[I"] = { query = "@conditional.outer", desc = "Prev conditional end" },
-						["[L"] = { query = "@loop.outer", desc = "Prev loop end" },
+						["[F"] = { query = "@call.outer", { desc = "Prev function call end" } },
+						["[M"] = { query = "@function.outer", { desc = "Prev method/function def end" } },
+						["[C"] = { query = "@class.outer", { desc = "Prev class end" } },
+						["[I"] = { query = "@conditional.outer", { desc = "Prev conditional end" } },
+						["[L"] = { query = "@loop.outer", { desc = "Prev loop end" } },
 					},
 				},
 			},
