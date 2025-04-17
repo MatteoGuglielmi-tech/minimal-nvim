95b8d1fb854775b8e860dd9c2027de3ebc8d2d2f
MatteoGuglielmi-Tech <matteo.guglielmi.ma.gu@gmail.com>
2025-04-17 21:43:00 +0200

    fix(treesitter-textobjects): add commented line

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -49,7 +49,7 @@ return {
 				keymaps = {
 					-- You can use the capture groups defined in textobjects.scm
 					["a="] = { query = "@assignment.outer", { desc = "Select outer part of an assignment" } },
-					-- ["i="] = { query = "@assignment.inner", { desc = "Select inner part of an assignment" } },
+					["i="] = { query = "@assignment.inner", { desc = "Select inner part of an assignment" } },
 					["l="] = { query = "@assignment.lhs", { desc = "Select left hand side of an assignment" } },
 					["r="] = { query = "@assignment.rhs", { desc = "Select right hand side of an assignment" } },
 
