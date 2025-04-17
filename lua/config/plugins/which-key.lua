9623b0d76939bf58d02558b7c4d0483d0578644a
MatteoGuglielmi-Tech <matteo.guglielmi.ma.gu@gmail.com>
2025-04-17 21:40:17 +0200

    build(wk): switch to opts, add buffer show buffer local keymaps

--- lua/config/plugins/which-key.lua
+++ lua/config/plugins/which-key.lua
@@ -1,17 +1,24 @@
 return {
 	"folke/which-key.nvim",
 	event = "VeryLazy",
-	config = function()
-		local wk = require("which-key")
-		wk.setup({
-			plugins = {
-				spelling = { enabled = true },
-				presets = {
-					operators = false,
-					motions = false,
-					windows = false,
-				},
+	opts = {
+		plugins = {
+			spelling = { enabled = true },
+			presets = {
+				operators = false,
+				motions = false,
+				windows = false,
 			},
-		})
-	end,
+		},
+	},
+
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
+	},
 }
