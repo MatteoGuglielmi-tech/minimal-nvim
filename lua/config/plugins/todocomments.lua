7b8a28654d6d382ff4cb33c0f688d505b1f3aa71
MatteoGuglielmi-Tech <matteo.guglielmi.ma.gu@gmail.com>
2025-05-12 11:03:17 +0200

    build(comments): add new keywords, refactor

--- lua/config/plugins/todocomments.lua
+++ lua/config/plugins/todocomments.lua
@@ -1,21 +1,21 @@
 return {
-  "folke/todo-comments.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    require("todo-comments").setup({
-      keywords = {
-      FIX = {
-        icon = " ", -- icon used for the sign, and in search results
-        color = "error", -- can be a hex color, or a named color (see below)
-        alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
-      },
-      TODO = { icon = " ", color = "info" },
-      HACK = { icon = " ", color = "warning", alt = { "KEY" } },
-      WARN = { icon = " ", color = "warning", alt = { "WARNING", "REMIND" } },
-      PERF = { icon = "", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-      NOTE = { icon = "", color = "hint", alt = { "INFO" } },
-      TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
-    },
-    })
-  end
+	"folke/todo-comments.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		require("todo-comments").setup({
+			keywords = {
+				FIX = { icon = " ", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE", "fix", "bug" } },
+				TODO = { icon = " ", color = "info", alt = { "todo" } },
+				HACK = { icon = " ", color = "warning", alt = { "KEY", "hack" } },
+				WARN = {
+					icon = " ",
+					color = "warning",
+					alt = { "WARNING", "REMIND", "warning", "reminder", "important" },
+				},
+				PERF = { icon = "", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE", "perf", "optimize", "improvement" } },
+				NOTE = { icon = "", color = "hint", alt = { "INFO", "note", "info" } },
+				TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED", "test" } },
+			},
+		})
+	end,
 }
