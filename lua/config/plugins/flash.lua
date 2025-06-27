086fab2ff1a93c10d8e5f4d87dacb7b0d08821f3
MatteoGuglielmi-Tech <matteo.guglielmi.ma.gu@gmail.com>
2025-06-27 14:38:54 +0200

    perf(flash): improve keys

--- lua/config/plugins/flash.lua
+++ lua/config/plugins/flash.lua
@@ -1,14 +1,17 @@
 return {
-  "folke/flash.nvim",
-  event = "VeryLazy",
-  ---@type Flash.Config
-  opts = {},
-  -- stylua: ignore
-  keys = {
-    { "S", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "T", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "m", mode = "o", function() require("flash").remote() end, desc = "remote flash" },
-    -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-  },
+	"folke/flash.nvim",
+	event = "VeryLazy",
+	---@type Flash.Config
+	opts = {},
+	-- stylua: ignore
+	keys = {
+	  -- jump() allows to jump between tags
+	  { "//", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+	  -- treesitter() allows to select and jump among semantic block parsed by treesitter
+	  { "S", mode = { "n" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+	  -- remote() takes a motion, performs and operation and then the cursor goes back to previous location
+	  -- e.g. ym<text to search><tag>y
+	  -- this motion allows to copy the entire line wherein <text to search> is locted and then go bakc to initial line
+	  { "m", mode = "o", function() require("flash").remote() end, desc = "remote flash" },
+	},
 }
