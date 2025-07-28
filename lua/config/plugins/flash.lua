b53aed4d0f253284b25a638bbb1cf52c17c79ff3
MatteoGuglielmi-tech <matteo.guglielmi.ma.gu@gmail.com>
2025-07-28 16:42:32 +0200

    fix(flash): change overlapping keymap

--- lua/config/plugins/flash.lua
+++ lua/config/plugins/flash.lua
@@ -6,12 +6,12 @@ return {
 	-- stylua: ignore
 	keys = {
 	  -- jump() allows to jump between tags
-	  { "//", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+	  { "|", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
 	  -- treesitter() allows to select and jump among semantic block parsed by treesitter
 	  { "S", mode = { "n" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
 	  -- remote() takes a motion, performs and operation and then the cursor goes back to previous location
 	  -- e.g. ym<text to search><tag>y
-	  -- this motion allows to copy the entire line wherein <text to search> is locted and then go bakc to initial line
+	  -- this motion allows to copy the entire line wherein <text to search> is located and then go back to initial line
 	  { "m", mode = "o", function() require("flash").remote() end, desc = "remote flash" },
 	},
 }
