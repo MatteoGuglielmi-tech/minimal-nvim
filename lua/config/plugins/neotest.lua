37a49c3fac274406014b7b6ea3a2f9a7715868a5
MatteoGuglielmi-tech <matteo.guglielmi.ma.gu@gmail.com>
2025-07-28 16:43:46 +0200

    perf(neotest): add and perf mappings

--- lua/config/plugins/neotest.lua
+++ lua/config/plugins/neotest.lua
@@ -19,29 +19,61 @@ return {
 					python = (vim.fn.expand("$CONDA_PREFIX")) .. "/bin/python",
 				}),
 				-- require("rustaceanvim.neotest"),
-
-				vim.keymap.set("n", "<leader>rt", function()
-					require("neotest").run.run()
-				end, { desc = "[R]un the nearest [T]est" }),
-				vim.keymap.set("n", "<leader>rf", function()
-					require("neotest").run.run(vim.fn.expand("%"))
-				end, { desc = "[R]un the current [F]ile" }),
-				vim.keymap.set("n", "<leader>dt", function()
-					require("neotest").run.run({ strategy = "dap" })
-				end, { desc = "[D]ebug the nearest [T]est" }),
-				vim.keymap.set("n", "<leader>st", function()
-					require("neotest").run.stop()
-				end, { desc = "[S]top the nearest [T]est" }),
-				vim.keymap.set("n", "<leader>at", function()
-					require("neotest").run.attach()
-				end, { desc = "[A]ttach the nearest [T]est" }),
-				vim.keymap.set("n", "<leader>ot", function()
-					require("neotest").output()
-				end, { desc = "Displays [O]utput of the [T]est" }),
-				vim.keymap.set("n", "<leader>wt", function()
-					require("neotest").watch()
-				end, { desc = "[W]atch files related to [T]ests for changes and re-run tests" }),
 			},
 		})
+
+		vim.keymap.set(
+			"n", "<leader>nr",
+			function() require("neotest").run.run() end,
+			{ desc = "[N]eotest: [r]un nearest test" }
+		)
+
+		vim.keymap.set("n", "<leader>nf",
+			function() require("neotest").run.run(vim.fn.expand("%")) end,
+			{ desc = "[N]eotest: run the current [f]ile" }
+		)
+
+		vim.keymap.set("n", "<leader>nd",
+			function()
+				require("neotest").run.run({vim.fn.expand("%"), strategy = "dap"})
+			end,
+			{ desc = "[N]eotest: [d]ebug the nearest test" }
+		)
+
+		vim.keymap.set("n", "<leader>ns",
+			function() require("neotest").run.stop() end,
+			{ desc = "[N]eotest: [s]top nearest test" }
+		)
+
+		vim.keymap.set("n", "<leader>na",
+			function() require("neotest").run.attach() end,
+			{ desc = "[N]eotst: [a]ttach the nearest test" }
+		)
+
+		vim.keymap.set("n", "<leader>no",
+			function() require("neotest").output() end,
+			{ desc = "[N]eotest: displays test [o]utput" }
+		)
+
+		vim.keymap.set("n", "<leader>nw",
+			function() require("neotest").watch() end,
+			{ desc = "[N]eotest: [w]atch files related to [t]ests for changes and re-run tests" }
+		)
+
+		vim.keymap.set("n", "<leader>ni",
+			function() require("neotest").summary.toggle() end,
+			{ desc = "[N]eotest: test [i]nfo (summary)" }
+		)
+
+		vim.keymap.set("n", "[n",
+			function() require("neotest").jump.prev({ status = "failed" }) end,
+			{ desc = "[N]eotest: jump to previous failed test" }
+		)
+
+		vim.keymap.set("n", "]n",
+			function() require("neotest").jump.next({ status = "failed" }) end,
+			{ desc = "[N]eotest: jump to next failed test" }
+		)
+
 	end,
 }
