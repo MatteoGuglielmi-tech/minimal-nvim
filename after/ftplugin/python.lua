767cc9319ed639eef936800a33dab79323d84c9a
MatteoGuglielmi-tech <matteo.guglielmi.ma.gu@gmail.com>
2025-08-03 14:52:32 +0200

    build: remove old mapping to trigger split term

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -3,13 +3,3 @@ vim.opt_local.expandtab = true
 vim.opt_local.shiftwidth = 4
 vim.opt_local.softtabstop = 4
 vim.opt_local.formatoptions:append({ c = true, r = true, o = true, q = true })
-
--- terminal
-local job_id = 0
-vim.keymap.set("n", "<space>st", function()
-	vim.cmd.vnew()
-	vim.cmd.term()
-	vim.cmd.wincmd("J")
-	vim.api.nvim_win_set_height(0, 15)
-	job_id = vim.bo.channel
-end, { desc = "Add terminal split" })
--- after/ftplugin/zig.lua
+++ after/ftplugin/zig.lua
@@ -5,14 +5,6 @@ vim.opt_local.formatoptions:append({ c = true, r = true, o = true, q = true })
 
 -- terminal
 local job_id = 0
-vim.keymap.set("n", "<space>st", function()
-	vim.cmd.vnew()
-	vim.cmd.term()
-	vim.cmd.wincmd("J")
-	vim.api.nvim_win_set_height(0, 15)
-	job_id = vim.bo.channel
-end, { desc = "Add terminal split" })
-
 vim.keymap.set("n", "<leader>ct", function()
 	vim.fn.chansend(job_id, { "zig run main.zig" })
 end)
