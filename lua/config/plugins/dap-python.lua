aaa9c8711f03fa602e9f1374a848de985ddc96df
MatteoGuglielmi-tech <matteo.guglielmi.ma.gu@gmail.com>
2025-08-06 15:26:09 +0200

    perf(nvim-dap-python): add path to environment, verbosity
    BREAKING CHANGE: now debugpy needs to be installed in the virtual
    environment

--- lua/config/plugins/dap-python.lua
+++ lua/config/plugins/dap-python.lua
@@ -1,13 +1,26 @@
+
 return {
 	"mfussenegger/nvim-dap-python",
-	dependencies = {
-		"mfussenegger/nvim-dap",
-	},
-	config = function()
-		local mason_path = vim.fn.glob(vim.fn.stdpath("data") .. "/mason/")
-		pcall(function()
-			-- /home/matteo/.local/share/nvim/mason/packages/debugpy/venv/bin
-			require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python")
-		end)
-	end,
+	dependencies = { "mfussenegger/nvim-dap" },
+	-- config = function()
+	-- 	pcall(function()
+	-- 		local python_path = lspconfig_utils.get_conda_python_path()
+	-- 		require("dap-python").setup(python_path)
+	-- 	end)
+	-- end,
+	config = function ()
+		local lspconfig_utils = require("config.lsp_utils.lsp_utils")
+		local python_path = lspconfig_utils.get_conda_python_path()
+
+		if python_path and vim.fn.executable(python_path) == 1 then
+			-- Notify setup is running with found path
+			vim.notify("✅ Found Conda Python for DAP: " .. python_path, vim.log.levels.INFO)
+			require("dap-python").setup(python_path)
+		else
+			vim.notify(
+				"❌ Conda Python not found or not executable. nvim-dap-python not configured.",
+				vim.log.levels.WARN
+			)
+		end
+	end
 }
