4cb3cd90b2f54ecb0b7099ee63b551786f03ff9f
MatteoGuglielmi-tech <matteo.guglielmi.ma.gu@gmail.com>
2025-08-14 21:41:55 +0200

    build: change virtual env switcher

---
+++ lua/config/plugins/venv_selector.lua
@@ -0,0 +1,26 @@
+return {
+  "linux-cultist/venv-selector.nvim",
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    "mfussenegger/nvim-dap",
+    "mfussenegger/nvim-dap-python",
+    {
+      "nvim-telescope/telescope.nvim",
+      branch = "0.1.x",
+      dependencies = { "nvim-lua/plenary.nvim" }
+    },
+  },
+  lazy = false,
+  branch = "regexp",
+  keys = {
+    { "<leader>vs", "<cmd>VenvSelect<CR>" },
+  },
+  opts = {
+    search = {
+      anaconda_base = {
+        command = "fd /bin/python$ " .. vim.fn.expand("$HOME/.softwares/miniforge3/envs") .. " --full-path --color never -E /proc",
+        type = "anaconda"
+      },
+    },
+  },
+}
