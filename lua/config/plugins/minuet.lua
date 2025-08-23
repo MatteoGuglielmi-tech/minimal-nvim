ffded04b8b85e83f4945d7df4e513864c4cad807
MatteoGuglielmi-Tech <matteo.guglielmi.ma.gu@gmail.com>
2025-08-23 15:10:14 +0200

    feat(minuet): add ai

--- lua/config/plugins/minuet.lua
+++ lua/config/plugins/minuet.lua
@@ -2,6 +2,7 @@
 
 return {
 	"milanglacier/minuet-ai.nvim",
+	enabled = false,
 	dependencies = { "nvim-lua/plenary.nvim" },
 	opts = function()
 		local utils = require("config.utils.utils")
@@ -17,11 +18,32 @@ return {
 			api_keys = utils.load_env(env_path) or {}
 		end
 
+		local gemini_key = (api_keys and api_keys.GEMINI_API_KEY) or os.getenv("GEMINI_API_KEY")
+		local deepseek_key = (api_keys and api_keys.DEEPSEEK_API_KEY) or os.getenv("DEEPSEEK_API_KEY")
+
+		local function wrap(key)
+			return "'" .. key .. "'"
+		end
+
 		return {
+			provider = "openai_compatible",
 			provider_options = {
+				-- Gemini --
 				gemini = {
-					model = "gemini-2.0-flash",
-					api_key = (api_keys and api_keys.GEMINI) or os.getenv("GEMINI_API_KEY"),
+					model = "gemini-1.5-pro-latest",
+					api_key = gemini_key,
+				},
+				-- DeepSeek --
+				openai_compatible = {
+					end_point = "https://api.deepseek.com/chat/completions",
+					api_key = deepseek_key,
+					name = "deepseek",
+					optional = {
+						model = "deepseek-chat", -- model
+						max_tokens = 4096,
+						temperature = 0.7,
+						top_p = 0.9,
+					},
 				},
 			},
 		}
