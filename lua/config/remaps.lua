58d904203cd32d3d964b38057f880018a982f653
MatteoGuglielmi-Tech <matteo.guglielmi.ma.gu@gmail.com>
2025-08-23 14:42:29 +0200

    feat(keymaps): add visualization for NeoVim messages

--- lua/config/remaps.lua
+++ lua/config/remaps.lua
@@ -1,10 +1,6 @@
 vim.keymap.set("n", "<space>", " ", {})
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-vim.keymap.set("n", "<space><space>x", function()
-	vim.cmd("source %")
-	print("Msg: current file successfully sourced via keymap")
-end, { desc = "Source current file" })
 
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
@@ -28,6 +24,77 @@ vim.keymap.set("n", "*", "*zz", { desc = "Find next occurrence of word under cur
 vim.keymap.set("n", "#", "#zz", { desc = "Find previous occurrence of word under cursor" })
 vim.keymap.set("n", "g*", "g*zz", { desc = "Find next occurrences of selected char" })
 vim.keymap.set("n", "g#", "g#zz", { desc = "Find previous occurrence of selected char" })
+
+-- visualize Neovim error
+vim.keymap.set("n", "<space>nm", function ()
+  -- capture `:messages` output
+  vim.cmd('redir @a | messages | redir END')
+  local message_history = vim.fn.getreg('a')
+
+  -- create empty buffer
+  local buf = vim.api.nvim_create_buf(false, true)
+  vim.bo[buf].buftype = 'nofile'
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, vim.split(message_history, '\n'))
+
+  -- ui calculation
+  local width = math.floor(vim.o.columns * 0.8)
+  local height = math.floor(vim.o.lines * 0.8)
+  local row = math.floor((vim.o.lines - height) / 2)
+  local col = math.floor((vim.o.columns - width) / 2)
+
+  -- open the floating window
+  vim.api.nvim_open_win(buf, true, {
+    relative = 'editor',
+    width = width,
+    height = height,
+    row = row,
+    col = col,
+    style = 'minimal',
+    border = 'rounded',
+  })
+
+  -- buffer-local keymap to close the window
+  vim.keymap.set('n', 'q', '<cmd>close!<CR>', { silent = true, buffer = buf })
+end, { desc = "Show [N]eovim [m]essage history in floating window" })
+
+
+
+
+vim.keymap.set("n", "<space>em", function()
+  local errmsg = vim.v.errmsg
+
+  if errmsg and errmsg ~= "" then
+    local buf = vim.api.nvim_create_buf(false, true)
+    vim.bo[buf].buftype = 'nofile'
+
+    local lines = vim.split(errmsg, '\n')
+    vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+
+    local width = math.floor(vim.o.columns * 0.8)
+    local height = math.min(math.floor(vim.o.lines * 0.8), #lines + 2)
+    local row = math.floor((vim.o.lines - height) / 2)
+    local col = math.floor((vim.o.columns - width) / 2)
+
+    vim.api.nvim_open_win(buf, true, {
+      relative = 'editor',
+      width = width,
+      height = height,
+      row = row,
+      col = col,
+      style = 'minimal',
+      border = 'rounded',
+    })
+
+    vim.keymap.set('n', 'q', '<cmd>close!<CR>', {
+      silent = true,
+      buffer = buf,
+    })
+  else
+    -- no error messages
+    vim.notify("No error message to display.", vim.log.levels.INFO)
+  end
+end, { desc = "Show [e]rror [m]essage in floating window" })
+
 
 -- yanking
 vim.keymap.set("n", "P", '"0p', { desc = "Yank from register 0" })
