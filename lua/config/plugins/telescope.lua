c5bd3d6e37fb1cd02c17b80449b83f11711aec72
MatteoGuglielmi-Tech <matteo.guglielmi.ma.gu@gmail.com>
2025-08-23 14:42:14 +0200

    chore(telescope): remove pcall

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -34,10 +34,11 @@ return {
 				["ui-select"] = { theme = "ivy" },
 			},
 		})
-		pcall(require("telescope").load_extension, "fzf")
-		pcall(require("telescope").load_extension, "ui-select")
-		pcall(require("telescope").load_extension, "bookmarks")
-		pcall(require("telescope").load_extension, "dap")
+
+		require("telescope").load_extension("fzf")
+		require("telescope").load_extension("ui-select")
+		require("telescope").load_extension("bookmarks")
+		require("telescope").load_extension("dap")
 
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
