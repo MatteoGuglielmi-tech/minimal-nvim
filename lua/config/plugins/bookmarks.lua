bcc8eda44259f69c0b5c19efc39f6af0d2199e62
MatteoGuglielmi-tech <matteo.guglielmi.ma.gu@gmail.com>
2025-08-25 16:12:25 +0200

    perf(bookmarks): improvements

--- lua/config/plugins/bookmarks.lua
+++ lua/config/plugins/bookmarks.lua
@@ -1,6 +1,6 @@
 return {
 	"tomasky/bookmarks.nvim",
-	dependencies = { "telescope.nvim" },
+	dependencies = { "nvim-telescope/telescope.nvim" },
 	event = "VimEnter",
 	config = function()
 		require("bookmarks").setup({
@@ -14,14 +14,19 @@ return {
 			---@diagnostic disable-next-line: unused-local
 			on_attach = function(bufnr)
 				local bm = require("bookmarks")
+
+				local callback_list = function()
+					require("telescope").extensions.bookmarks.list()
+				end
+
 				local map = vim.keymap.set
-				map("n", "bm", bm.bookmark_toggle) -- add or remove bookmark at current line
-				map("n", "bi", bm.bookmark_ann) -- add or edit mark annotation at current line
-				map("n", "bc", bm.bookmark_clean) -- clean all marks in local buffer
-				map("n", "bn", bm.bookmark_next) -- jump to next mark in local buffer
-				map("n", "bp", bm.bookmark_prev) -- jump to previous mark in local buffer
-				map("n", "bl", bm.bookmark_list) -- show marked file list in quickfix window
-				map("n", "bx", bm.bookmark_clear_all) -- removes all bookmarks
+				map("n", "<leader>bm", bm.bookmark_toggle) -- add or remove bookmark at current line
+				map("n", "<leader>bi", bm.bookmark_ann) -- add or edit mark annotation at current line
+				map("n", "<leader>bc", bm.bookmark_clean) -- clean all marks in local buffer
+				map("n", "<leader>bn", bm.bookmark_next) -- jump to next mark in local buffer
+				map("n", "<leader>bp", bm.bookmark_prev) -- jump to previous mark in local buffer
+				map("n", "<leader>bl", callback_list) -- show marked file list in quickfix window
+				map("n", "<leader>bx", bm.bookmark_clear_all) -- removes all bookmarks
 			end,
 		})
 	end,
