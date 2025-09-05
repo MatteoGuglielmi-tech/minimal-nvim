81152512e47d6105cfbd5d2e43289d45c2d580a0
MatteoGuglielmi-Tech <matteo.guglielmi.ma.gu@gmail.com>
2025-09-05 11:36:50 +0200

    fix,feat(dap-lldb, rustaceanvim): fix `rust-analyzer` overwriting, enable `dap-lldb`

--- lua/config/plugins/dap-lldb.lua
+++ lua/config/plugins/dap-lldb.lua
@@ -1,6 +1,41 @@
 return {
 	"julianolf/nvim-dap-lldb",
-	enabled = false,
 	dependencies = { "mfussenegger/nvim-dap" },
-	opts = {},
+	ft = { "rust" },
+	config = function()
+		local dap = require("dap")
+		local dap_lldb = require("dap-lldb")
+
+		-- register the adapter
+		dap.adapters.codelldb = {
+			type = "server",
+			port = "${port}",
+			executable = {
+				-- command = codelldb_path,
+				command = "/Users/matteoguglielmi/.local/share/nvim/mason/packages/codelldb/codelldb",
+				args = { "--port", "${port}" },
+			},
+		}
+
+		-- rust
+		dap.configurations.rust = {
+			{
+				name = "Launch file",
+				type = "codelldb",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+				stopOnEntry = false,
+			},
+		}
+
+		-- dap.configurations.cpp = dap.configurations.rust
+		-- dap.configurations.c = dap.configurations.rust
+
+		vim.keymap.set("n", "<leader>drt", function()
+			dap_lldb.debug_test()
+		end, { desc = "[D]ap: debug [r]ust [t]est" })
+	end,
 }
--- lua/config/plugins/rustaceanvim.lua
+++ lua/config/plugins/rustaceanvim.lua
@@ -1,64 +1,51 @@
 return {
 	"mrcjkb/rustaceanvim",
-	version = "^5",
+	version = "^6",
+	lazy = false,
 	init = function()
 		vim.g.rustaceanvim = {
-			-- LSP configuration
-			server = {
-				cmd = function()
-					local mason_registry = require("mason-registry")
-					if mason_registry.is_installed("rust-analyzer") then
-						-- This may need to be tweaked depending on the operating system.
-						local ra = mason_registry.get_package("rust-analyzer")
-						local ra_filename = ra:get_receipt():get().links.bin["rust-analyzer"]
-						return { ("%s/%s"):format(ra:get_install_path(), ra_filename or "rust-analyzer") }
-					else
-						-- global installation
-						return { "rust-analyzer" }
-					end
-				end,
-				settings = {
-					-- rust-analyzer language server configuration
-					["rust-analyzer"] = {
-						inlayHints = {
-							bindingModeHints = {
-								enable = false,
-							},
-							chainingHints = {
-								enable = true,
-							},
-							closingBraceHints = {
-								enable = true,
-								minLines = 25,
-							},
-							closureReturnTypeHints = {
-								enable = "never",
-							},
-							lifetimeElisionHints = {
-								enable = "never",
-								useParameterNames = false,
-							},
-							maxLength = 25,
-							parameterHints = {
-								enable = true,
-							},
-							reborrowHints = {
-								enable = "never",
-							},
-							renderColons = true,
-							typeHints = {
-								enable = true,
-								hideClosureInitialization = false,
-								hideNamedConstructor = false,
-							},
-							checkOnSave = false,
+			settings = {
+				["rust-analyzer"] = {
+					check = {
+						command = "clippy",
+					},
+					inlayHints = {
+						bindingModeHints = {
+							enable = false,
 						},
+						chainingHints = {
+							enable = true,
+						},
+						closingBraceHints = {
+							enable = true,
+							minLines = 25,
+						},
+						closureReturnTypeHints = {
+							enable = "never",
+						},
+						lifetimeElisionHints = {
+							enable = "never",
+							useParameterNames = false,
+						},
+						maxLength = 25,
+						parameterHints = {
+							enable = true,
+						},
+						reborrowHints = {
+							enable = "never",
+						},
+						renderColons = true,
+						typeHints = {
+							enable = true,
+							hideClosureInitialization = false,
+							hideNamedConstructor = false,
+						},
+						checkOnSave = false,
 					},
 				},
+				-- DAP configuration
+				-- dap = {},
 			},
-			-- DAP configuration
-			-- dap = {},
 		}
 	end,
-	lazy = false,
 }
