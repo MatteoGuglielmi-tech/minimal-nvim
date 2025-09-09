5aa6d9bb70fe6a5018b2371d460f08e3c81b67fc
MatteoGuglielmi-tech <matteo.guglielmi.ma.gu@gmail.com>
2025-09-09 11:42:23 +0200

    chore(rust): add more verbose descriptions for keymaps

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -6,32 +6,32 @@ vim.opt_local.formatoptions:append({ c = true, r = true, o = true, q = true })
 local bufnr = vim.api.nvim_get_current_buf()
 vim.keymap.set("n", "<leader>rca", function()
 	vim.cmd.RustLsp("codeAction")
-end, { silent = true, buffer = bufnr, desc = "[C]ode [A]ction for Rust" })
+end, { silent = true, buffer = bufnr, desc = "[R]ust: [c]ode [a]ction" })
 
 -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
 vim.keymap.set("n", "K", function()
 	vim.cmd.RustLsp({ "hover", "actions" })
-end, { silent = true, buffer = bufnr, desc = "[H]over for Rust" })
+end, { silent = true, buffer = bufnr, desc = "Rust: hover [K]" })
 
 vim.keymap.set("n", "<leader>rem", function()
 	vim.cmd.RustLsp("expandMacro")
-end, { silent = true, buffer = bufnr, desc = "[R]ust [E]xpand [M]acro" })
+end, { silent = true, buffer = bufnr, desc = "[R]ust: [e]xpand [m]acro" })
 
 vim.keymap.set("n", "<leader>rmu", function()
 	vim.cmd.RustLsp({ "moveItem", "up" })
-end, { silent = true, buffer = bufnr, desc = "[R]ust [M]ove item [U]p" })
+end, { silent = true, buffer = bufnr, desc = "[R]ust: [m]ove item [u]p" })
 
 vim.keymap.set("n", "<leader>rmd", function()
 	vim.cmd.RustLsp({ "moveItem", "down" })
-end, { silent = true, buffer = bufnr, desc = "[R]ust [M]ove item [D]own" })
+end, { silent = true, buffer = bufnr, desc = "[R]ust: [m]ove item [d]own" })
 
 vim.keymap.set("n", "<leader>ree", function()
 	vim.cmd.RustLsp({ "explainError", "cycle" })
-end, { silent = true, buffer = bufnr, desc = "[R]ust [E]xplain [E]rror" })
+end, { silent = true, buffer = bufnr, desc = "[R]ust: [e]xplain [e]rror" })
 
 vim.keymap.set("n", "<leader>rrd", function()
 	vim.cmd.RustLsp({ "renderDiagnostic", "cycle" })
-end, { silent = true, buffer = bufnr, desc = "[R]ust [R]ender [D]iagnostics" })
+end, { silent = true, buffer = bufnr, desc = "[R]ust: [r]ender [d]iagnostics" })
 
 vim.keymap.set("n", "<leader>rjr", function()
 	vim.cmd.RustLsp("relatedDiagnostics")
@@ -39,8 +39,8 @@ end, { silent = true, buffer = bufnr, desc = "[R]ust [J]ump to [R]elated Diagnos
 
 vim.keymap.set("n", "<leader>rjl", function()
 	vim.cmd.RustLsp("joinLines")
-end, { silent = true, buffer = bufnr, desc = "[R]ust [J]oin [L]ines" })
+end, { silent = true, buffer = bufnr, desc = "[R]ust: [j]oin [l]ines" })
 
 vim.keymap.set("n", "<leader>rfc", function()
 	vim.cmd.RustLsp("flyCheck")
-end, { silent = true, buffer = bufnr, desc = "[R]ust [F]ly [C]heck" })
+end, { silent = true, buffer = bufnr, desc = "[R]ust: [f]ly [c]heck" })
