b59795167be1acc836547d64c81a54d7fe866ba5
MatteoGuglielmi-Tech <matteo.guglielmi.ma.gu@gmail.com>
2026-01-03 14:23:56 +0100

    fix(texlab): fix lsp config for `texlab`

--- after/lsp/texlab.lua
+++ after/lsp/texlab.lua
@@ -1,3 +1,28 @@
 return {
-	filetypes = { "latex" },
+	cmd = { "texlab" },
+	filetypes = { "tex", "plaintex", "bib" },
+	settings = {
+		texlab = {
+			bibtexFormatter = "texlab",
+			build = {
+				args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" },
+				executable = "latexmk",
+				forwardSearchAfter = false,
+				onSave = false,
+			},
+			chktex = {
+				onEdit = false,
+				onOpenAndSave = false,
+			},
+			diagnosticsDelay = 300,
+			formatterLineLength = 80,
+			forwardSearch = {
+				args = {},
+			},
+			latexFormatter = "latexindent",
+			latexindent = {
+				modifyLineBreaks = false,
+			},
+		},
+	},
 }
