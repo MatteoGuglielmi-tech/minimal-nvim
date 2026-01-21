a7b2390758735f2c539e4ab9acda12ed096daba6
MatteoGuglielmi-tech <matteo.guglielmi.ma.gu@gmail.com>
2026-01-21 19:31:46 +0100

    feat(telescope): add keymap for telescope diagnostics

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -47,8 +47,11 @@ return {
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]iles" })
 		vim.keymap.set("n", "<leader>fs", builtin.builtin, { desc = "[F]ind [S]elect Telescope" })
 		vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[F]ind current [W]ord" })
-		-- vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind by [G]rep" })
-		vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[F]ind [D]iagnostics" })
+		vim.keymap.set("n", "<leader>wd", builtin.diagnostics, { desc = "[W]orkspace [D]iagnostics" })
+		vim.keymap.set("n", "<leader>bd", function()
+			builtin.diagnostics({ bufnr = 0 })
+		end, { desc = "[B]uffer [D]iagnostics" })
+
 		-- vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[F]ind [R]esume" })
 		vim.keymap.set("n", "<leader>f.", builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
 		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
