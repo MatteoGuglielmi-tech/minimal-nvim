987d379baeba1b0933fcf9c7a20fa502c0a91a4f
MatteoGuglielmi-tech <matteo.guglielmi.ma.gu@gmail.com>
2026-01-21 19:32:42 +0100

    build(trouble): remove unused keymaps

--- lua/config/plugins/trouble.lua
+++ lua/config/plugins/trouble.lua
@@ -1,19 +1,14 @@
 return {
 	{
 		"folke/trouble.nvim",
-		-- dependencies = { "echasnovski/mini.icons" },
+		dependencies = { "nvim-telescope/telescope.nvim"},
 		opts = {},
 		cmd = "Trouble",
 		keys = {
-			{ "<leader>td", "<cmd>Trouble diagnostics<CR>", { desc = "Document Diagnostic (Trouble)" } },
-			{
-				"<leader>tc",
-				"<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
-				{ desc = "Buffer Diagnostics (Trouble)" },
-			},
-			{ "<leader>tl", "<cmd>Trouble loclist toggle<CR>", { desc = "Loclist Toggle (Trouble)" } },
-			{ "<leader>tq", "<cmd>Trouble qflist toggle<CR>", { desc = "Quickfix list (Trouble)" } },
-			{ "<leader>tt", "<cmd>Trouble todo<CR>", { desc = "Toggle TODO (Trouble)" } },
+			{ "<leader>tw", "<cmd>Trouble todo<CR>", { desc = "[T]rouble TODO [W]orkspace" } },
+			{ "<leader>tt", "<cmd>TodoTelescope<CR>", { desc = "[T]rouble TODO [T]elescope workspace" } },
+			{ "<leader>ttb", "<cmd>Trouble todo focus=false filter.buf=0<CR>", { desc = "[T]rouble [T]oggle TODO current [B]uffer" } },
 		},
 	},
 }
+
