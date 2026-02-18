83d81579dcbb05acd4ab66e4b72d0a58865aab51
MatteoGuglielmi-Tech <matteo.guglielmi.ma.gu@gmail.com>
2026-02-18 16:51:29 +0100

    feat: solve keystrokes conflicts

--- lua/config/plugins/comment.lua
+++ lua/config/plugins/comment.lua
@@ -2,12 +2,28 @@ return {
 	"numToStr/Comment.nvim",
 	event = "VeryLazy",
 	config = function()
-		require("Comment").setup()
+		require("Comment").setup({
+			mappings = {
+				basic = false,
+				extra = false,
+			},
+		})
+
+		--- normal mode
 		vim.keymap.set(
 			"n",
-			"<leader>/",
+			"gcc",
 			'<cmd>lua require("Comment.api").toggle.linewise.current()<CR>',
 			{ desc = "Comment line" }
 		)
+		vim.keymap.set(
+			"n",
+			"gca",
+			'<cmd>lua require("Comment.api").insert.linewise.eol()<CR>',
+			{ desc = "Append comment to eol" }
+		)
+
+		--- Visual mode
+		vim.keymap.set("x", "gc", "<Plug>(comment_toggle_linewise_visual)")
 	end,
 }
